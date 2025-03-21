04a935b62a7029f3b7ceefdea8db3ee608a22ca6
HudsonGraeme <spencer@inferencelabs.com>
2025-03-21 02:28:54 +0000

    Update deployment script

--- contracts/anvil/build-state.sh
+++ contracts/anvil/build-state.sh
@@ -5,7 +5,7 @@ STATE_FILE="contracts/anvil/state.json"
 mkdir -p "$(dirname "$STATE_FILE")"
 
 echo "Starting Anvil with state dump in background"
-anvil --dump-state "$STATE_FILE" --host 0.0.0.0 --port 8545 --base-fee 0 --gas-price 0 > /dev/null 2>&1 &
+anvil --dump-state "$STATE_FILE" --host 0.0.0.0 --port 8545 --base-fee 0 --gas-price 0 &
 ANVIL_PID=$!
 
 sleep 3
@@ -14,13 +14,13 @@ cp .env.example .env
 cp contracts/.env.example contracts/.env
 
 echo "Building contracts"
-make build-contracts > /dev/null 2>&1
+make build-contracts
 
 echo "Deploying EigenLayer contracts."
-make deploy-eigenlayer-contracts > /dev/null 2>&1
+make deploy-eigenlayer-contracts
 
 echo "Deploying Sertn contracts."
-make deploy-sertn-contracts > /dev/null 2>&1
+make deploy-sertn-contracts
 
 echo "Killed Anvil"
 kill $ANVIL_PID
