b9c1c57ecb790f4a6f5a57cafddff2d557b7d11c
Bondarenko Oleg <millioner.bbb@gmail.com>
2025-04-09 23:23:18 +0300

    Python avs (#10)
    * Some settings and configs for python code
    
    * Sample code to work on
    
    * cspell update
    
    * Aggregator and operator improvements
    
    * Aggregator and operator in progress
    
    * Move ABI generating to 'contract compile' command
    
    * 'registerOperator' function call finally seems to be working
    
    * Operator/aggregator fixes
    
    * Move existing logic to 'client' folder. Start using typer, console and yaml config
    
    * Move operator logic to new place
    
    * Some refactoring for operator
    
    * Move aggregator logic to new place
    
    * Tiny adjustments for readme

--- client/src/common/eth.py
+++ client/src/common/eth.py
@@ -1,10 +1,41 @@
-from web3 import Web3
+import json
 from typing import Optional
+
+from eth_account import Account
+from web3 import Web3
+
+from common.abis import SERVICE_MANAGER_ABI, TASK_MANAGER_ABI
+from common.constants import (
+    SERVICE_MANAGER_ADDRESS,
+    TASK_MANAGER_ADDRESS,
+)
+from common.console import console, styles
+
+
+def load_ecdsa_private_key(keystore_path: str, password: str) -> str:
+    """
+    Load ECDSA private key from keystore file
+    """
+    if not password:
+        console.print(
+            "ECDSA key not set. using empty string.",
+            style=styles.debug,
+        )
+
+    with open(keystore_path, "r") as f:
+        keystore = json.load(f)
+    return Account.decrypt(keystore, password).hex()
 
 
 class EthereumClient:
     def __init__(self, rpc_url: Optional[str] = None):
         self.w3 = Web3(Web3.HTTPProvider(rpc_url or "http://localhost:8545"))
+        self.service_manager = self.w3.eth.contract(
+            address=SERVICE_MANAGER_ADDRESS, abi=SERVICE_MANAGER_ABI
+        )
+        self.task_manager = self.w3.eth.contract(
+            address=TASK_MANAGER_ADDRESS, abi=TASK_MANAGER_ABI
+        )
 
     def is_connected(self) -> bool:
         return self.w3.is_connected()
